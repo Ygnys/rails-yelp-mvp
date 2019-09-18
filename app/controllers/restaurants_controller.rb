ad0a424742ca9f4ca70ace92c1f22c6f55db2335
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-18 16:54:02 +0200

    rake passes but some bugs in the app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,18 @@ def new
 end
 
 def create
-  @restaurant = something
+  @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
 end
 
+private
+
+  def restaurant_params
+
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
-
-  get "restaurants", to: "restuarants#index"
-  get "restaurants/new", to: "restaurants#show"
-  post "restaurants", to: "restuarants#create"
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [:new, :create]
   end
 end
-
