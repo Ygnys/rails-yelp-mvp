c4029e372e6b6d4577473bedd410fb41c305d64c
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-18 12:00:42 +0200

    created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_092659) do
+ActiveRecord::Schema.define(version: 2019_09_18_095338) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "rating"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
